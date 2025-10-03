e0f69f0d5c540300baad0261e5a2a60cd78470b0
Kushagra <ay85166@gmail.com>
2025-10-03 17:24:27 +0530

    Some more comments

--- DS/Snippets/Linked_list/using_arrays.c
+++ DS/Snippets/Linked_list/using_arrays.c
@@ -9,7 +9,7 @@ typedef struct node{
 
 node list[max];
 int head=-1; // to symbolize empty list
-int freeindex=0;
+int freeindex=0; // to keep track of next empty index
 
 
 
@@ -17,8 +17,11 @@ int freeindex=0;
 
 //? inserting at the back 
 void insertback(int val){
+    // node is created
     list[freeindex].val = val;
     list[freeindex].next=-1;
+
+    // now we need to link the node
     if (head==-1){
         head=freeindex;
     }
@@ -40,14 +43,14 @@ void insert(int pos , int val){
     if (pos == 0){ // inserts at beginning
         list[freeindex].val=val;
         list[freeindex].next=pos;
-        head=freeindex;
+        head=freeindex; // we are inserting at beginning so head needs to be updated
     }
     else{ // inserts at any given position(index)
-        int curr = head , curr1=head;
-        int count=0,prev=-1;
-        while(curr!=-1 && count < pos){
-            prev=curr;
-            curr = list[curr].next;
+        int curr = head;
+        int count=0,prev=-1;                        // we used count and prev to keep track of how many nodes we need to cross 
+        while(curr!=-1 && count < pos){             // inorder to reach the required node
+            prev=curr;                              // since nodes are not inserted in sequential order we need to traverse through each node
+            curr = list[curr].next;                 // and count how many nodes we crossed to land at correct index 
             count++;
         }
         list[freeindex].val=val;
@@ -61,7 +64,19 @@ void insert(int pos , int val){
 
 //? Deleting a node
 void delete(int pos){
-    
+    if (pos == 0 &&  head == -1) printf("underflow");
+    else if(pos == 0  && head !=-1){
+        head=list[pos].next;
+    }
+    else{
+        int curr = head , count = 0 , prev = -1;
+        while(curr != -1 && count<pos){
+            prev = curr;
+            curr=list[curr].next;
+            count++;
+        }
+        list[prev].next=list[curr].next;
+    } 
 }
 
 //? searching for a node
@@ -99,19 +114,22 @@ int main(){
         i++;
     }
 
-    //inserting in middle
-    insert(1,78);
-    display();
+    // inserting nodes
+    insert(1,78); display();
 
-    insertback(89);
-    display();
+    insertback(89); display();
 
-    insert(2,56);
-    display();
+    insert(2,56); display();
  
-    
     // searching for a node
-    printf("Node no. is %d ",search(89));
+    printf("Node no. is %d\n",search(89));
+
+    // deleting nodes 
+    delete(0); display(); 
+    delete(2); display(); 
+    delete(2); display(); 
+    
+   
 
 }
 
