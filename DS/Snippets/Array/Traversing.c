71f5398b3c8d1bd1343516cbb146301d59a45807
Kushagra7536 <ay85166@gmail.com>
2025-10-05 17:40:30 +0530

    Update Traversing.c
    =

--- DS/Snippets/Array/Traversing.c
+++ DS/Snippets/Array/Traversing.c
@@ -6,7 +6,7 @@ int main(){
 
     int lower_bound=0 , upper_bound=4;
 
-    for (int i=lower_bound ; i<upper_bound ; i++){ // set i=lower_bound , updating i until i<=upper_bound
+    for (int i=lower_bound ; i<=upper_bound ; i++){ // set i=lower_bound , updating i until i<=upper_bound
         printf("%d ",a[i]);
     }
 
