8fc45871700f8ef28b6bb35437bad643d2a97d54
Kushagra <ay85166@gmail.com>
2025-10-19 01:23:56 +0530

    Java

---
+++ Java/Lab/Method_Overloading_and_Overriding/Test.java
@@ -0,0 +1,59 @@
+class Add{
+    int x,y,z,w ;
+
+// Method Overloading :: more than one Method of same name present in the same class 
+//                       identification on the basis of list of arguemnets 
+    int add(int a , int b){                        
+        return a+b;
+    }
+    int add(int a , int b , int c){
+        return a+b+c;
+    }
+    int add(int a , int b , int c , int d){            
+        return a+b+c+d;
+    }
+
+    void display(){
+        System.out.println(this.x+"\t"+this.y+"\t"+this.z+"\t"+this.w);
+    }
+
+    Add(int x , int y , int z , int w){  // constructor
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+}
+
+class B extends Add{
+    int a,b,c,d;
+// Method Overriding :: same method in both parent and child class but upon calling child class's function definition is called
+//                      same prototype should be there in boyh parent and child class 
+
+    void display(){
+        System.out.println(this.a+"\t"+this.b+"\t"+this.c+"\t"+this.d);
+    }
+
+    B(int x , int y , int z , int w , int a , int b , int c , int d){  // constructor
+        super(x,y,z,w);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+}
+
+
+class Test{
+    public static void main(String args[]){
+        Add obj = new Add(1,2,3,4);
+        Add obj1 = new B(1,2,3,4,5,6,7,8);   // Upcasting 
+        obj1.display();
+
+        System.out.println(obj1.x);             // we can also access instance variables 
+                                               // of the parent class 
+        System.out.println(obj1.y);           // while upcasting 
+        System.out.println(obj1.z);          //
+        System.out.println(obj1.w);         //
+    }
+}
